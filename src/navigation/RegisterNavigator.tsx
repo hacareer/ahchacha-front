4415ce0e22a305c2aa5e0c569d7b5a2106d05a64
nant0313 <nant0313@gmail.com>
2021-11-03 00:06:13 +0900

    Add screen: register page

--- src/navigation/RegisterNavigator.tsx
+++ src/navigation/RegisterNavigator.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {RegisterParamList} from '../../types/types';
 import RegisterMainScreen from '../screens/AppScreens/RegisterScreens/RegisterMainScreen';
 import RegisterHangOutsScreen from '../screens/AppScreens/RegisterScreens/RegisterHangOutsScreen';
@@ -7,11 +6,83 @@ import RegisterSchoolScreen from '../screens/AppScreens/RegisterScreens/Register
 import RegisterVaccinatedScreen from '../screens/AppScreens/RegisterScreens/RegisterVaccinatedScreen';
 import RegisterNicknameScreen from '../screens/AppScreens/RegisterScreens/RegisterNicknameScreen';
 import RegisterTermsScreen from '../screens/AppScreens/RegisterScreens/RegisterTermsScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Animated, View} from 'react-native';
+let cp = 'Main';
+const RegisterProgress = createStackNavigator();
+export default function RegisterProgressNavigator({navigation, route}) {
+  const [progressValue, setProgressValue] = React.useState(1);
+  const animatingProgressBar = React.useRef(new Animated.Value(0));
+  React.useEffect(() => {
+    Animated.timing(animatingProgressBar.current, {
+      toValue: progressValue,
+      // delay: animDelay,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, [progressValue]);
 
-const Register = createBottomTabNavigator<RegisterParamList>();
-export default function RegisterNavigator() {
+  const ProgressBar = (
+    <Animated.View
+      style={{
+        backgroundColor: 'yellow',
+        width: animatingProgressBar.current,
+        height: 8,
+      }}
+    />
+  );
+  React.useEffect(() => {
+    switch (cp) {
+      case 'Main':
+        console.log('running...');
+        // animatingProgressBar.current.setValue(1);
+        setProgressValue(1);
+        break;
+      case 'Terms':
+        console.log('running...');
+        setProgressValue(2);
+        break;
+      case 'Nickname':
+        console.log('running...');
+        setProgressValue(3);
+        break;
+      case 'Vaccinated':
+        console.log('running...');
+        setProgressValue(4);
+        break;
+      case 'School':
+        console.log('running...');
+        setProgressValue(5);
+        break;
+      case 'HangOuts':
+        console.log('running...');
+        setProgressValue(6);
+        break;
+    }
+  }, [navigation, route]);
   return (
-    <Register.Navigator>
+    <RegisterProgress.Navigator
+      screenOptions={{
+        header: () => ProgressBar,
+      }}>
+      <RegisterProgress.Screen
+        name="RegisterNav"
+        component={RegisterNavigator}
+      />
+    </RegisterProgress.Navigator>
+  );
+}
+interface IRegisterNavigationProps {
+  setCurrentRoute: React.Dispatch<React.SetStateAction<string>>;
+}
+const Register = createStackNavigator<RegisterParamList>();
+function RegisterNavigator(props: IRegisterNavigationProps) {
+  return (
+    <Register.Navigator
+      screenOptions={({route}) => {
+        cp = route.name;
+        return {headerShown: false};
+      }}>
       <Register.Screen name="Main" component={RegisterMainScreen} />
       <Register.Screen name="Terms" component={RegisterTermsScreen} />
       <Register.Screen name="Nickname" component={RegisterNicknameScreen} />
--- src/screens/AppScreens/RegisterScreens/RegisterMainScreen.tsx
+++ src/screens/AppScreens/RegisterScreens/RegisterMainScreen.tsx
@@ -1,6 +1,24 @@
+import {useNavigation} from '@react-navigation/native';
+import {StackScreenProps} from '@react-navigation/stack';
 import * as React from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
+import {RegisterParamList} from '../../../../types/types';
+import {Button} from '../../../components';
+interface IProps {}
+type Props = StackScreenProps<RegisterParamList, 'Main'>;
 
-export default function RegisterMainScreen() {
-  return <View />;
+export default function RegisterMainScreen(props: Props & IProps) {
+  const {route} = props;
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <Text>qoweihegiw</Text>
+      <Text>qoweihegiw</Text>
+      <Text>qoweihegiw</Text>
+      <Text>qoweihegiw</Text>
+      <Text>qoweihegiw</Text>
+      <Button text="다음" onPress={() => navigation.navigate('Terms')} />
+    </View>
+  );
 }
