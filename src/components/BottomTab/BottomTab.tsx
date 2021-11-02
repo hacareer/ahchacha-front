db8e79e998c5e1924f9d1c1699267e57b67abfff
nant0313 <nant0313@gmail.com>
2021-11-03 00:03:59 +0900

    Set config: BottomTab navigator

---
+++ src/components/BottomTab/BottomTab.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import {View} from 'react-native';
+
+export default function BottomTab(props) {
+    
+  return <View />;
+}
--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -5,11 +5,15 @@ import TabMainScreen from '../screens/AppScreens/TabScreens/TabMainScreen';
 import TabMapScreen from '../screens/AppScreens/TabScreens/TabMapScreen';
 import TabCalendarScreen from '../screens/AppScreens/TabScreens/TabCalendarScreen';
 import TabSchoolScreen from '../screens/AppScreens/TabScreens/TabSchoolScreen';
+import BottomTab_ from '../components/BottomTab/BottomTab';
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 export default function BottomTabNavigator() {
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator
+      screenOptions={{lazy: false}}
+      // tabBar={props => <BottomTab_ {...props} />}
+      >
       <BottomTab.Screen name="Main" component={TabMainScreen} />
       <BottomTab.Screen name="Map" component={TabMapScreen} />
       <BottomTab.Screen name="Calendar" component={TabCalendarScreen} />
