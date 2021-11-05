7cf81b24e81373b2ec48a5bb92cfce11fbe5b8ec
nant0313 <nant0313@gmail.com>
2021-11-05 10:29:09 +0900

    Add deeplinks

--- LinkingConfiguration.ts
+++ LinkingConfiguration.ts
@@ -2,6 +2,19 @@ import {ILinkingConfiguration} from './types/types';
 
 const config: ILinkingConfiguration = {
   prefixes: [],
-  config: {},
+  config: {
+    screens: {
+      Hospital: {
+        screens: {
+          Detail: {
+            path: 'hospital/detail/:hospitalID',
+            parse: {
+              key: (hospitalID: number) => `hospital-detail-${hospitalID}`,
+            },
+          },
+        },
+      },
+    },
+  },
 };
 export default config;
