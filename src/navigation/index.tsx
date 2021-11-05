962ab4f0f707a1282d55e2816524f6b4baa5e62d
nant0313 <nant0313@gmail.com>
2021-11-05 10:37:56 +0900

    Add pages: Hospital

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {RootStackParamList} from '../../types/types';
 import BottomTabNavigator from './BottomTabNavigator';
 import RegisterNavigator from './RegisterNavigator';
+import HospitalNavigator from './HospitalNavigator';
 
 const Stack = createStackNavigator<RootStackParamList>();
 export default function RootNavigator() {
@@ -10,6 +11,7 @@ export default function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={screenOptions}>
       <Stack.Screen name="Register" component={RegisterNavigator} />
+      <Stack.Screen name="Hospital" component={HospitalNavigator} />
       <Stack.Screen name="BottomTab" component={BottomTabNavigator} />
     </Stack.Navigator>
   );
