c3c9aeac5460e4b1de048866a2d061d2d9d525a4
nant0313 <nant0313@gmail.com>
2021-11-05 10:35:44 +0900

    Add global colors

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,7 +1,19 @@
-export const colors = {
-  primary: '#3F51B5',
-  accent: '#AF2923',
-  containerBg: '#FFF',
-  textColor: '#000',
-  borderColor: '#ccc',
+export default {
+  dark: {
+    pressedColor: '#f2f2f2',
+    disabledBackground: '#5C5C5C',
+    disabledColor: '#666666',
+  },
+  light: {
+    pressedColor: '#f2f2f2',
+    primary: '#3F51B5',
+    accent: '#AF2923',
+    containerBg: '#FFF',
+    textColor: '#000',
+    borderColor: '#ccc',
+    disabledBackground: '#CCCCCC',
+    disabledColor: '#666666',
+  },
 };
+
+export const transparent = 'rgba(0,0,0,0)';
